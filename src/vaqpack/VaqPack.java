aab21efb9030f54a11563793fbfdfcaabad90d28
chinomatic <chinomatic1970@gmail.com>
2015-11-04 21:33:27 -0600

    Version with database connection test

--- src/vaqpack/VaqPack.java
+++ src/vaqpack/VaqPack.java
@@ -5,6 +5,13 @@
  */
 package vaqpack;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.DriverManager;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.scene.Scene;
@@ -17,7 +24,7 @@ import javafx.stage.Stage;
  * @author OWNER
  */
 public class VaqPack extends Application {
-    
+
     @Override
     public void start(Stage primaryStage) {
         Button btn = new Button();
@@ -25,12 +32,12 @@ public class VaqPack extends Application {
         btn.setOnAction((ActionEvent event) -> {
             System.out.println("Hello World!");
         });
-        
+
         StackPane root = new StackPane();
         root.getChildren().add(btn);
-        
+
         Scene scene = new Scene(root, 300, 250);
-        
+
         primaryStage.setTitle("Hello Dreaming Java");
         primaryStage.setScene(scene);
         primaryStage.show();
@@ -40,7 +47,41 @@ public class VaqPack extends Application {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        launch(args);
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost/mytest?" + "user=root&password=xeno");
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery("SELECT * FROM untitled");
+            ResultSetMetaData metadata = resultSet.getMetaData();
+            int colCount = metadata.getColumnCount();
+            System.out.println("This is a connection test\n");
+            for (int i = 1; i <= colCount; i++) {
+                System.out.printf("%-8s\t", metadata.getColumnName(i));
+                
+            }
+            while (resultSet.next()) {
+                for (int i = 1; i <= colCount; i++) {
+                    System.out.printf("%-8s\t", resultSet.getObject(i));
+                    System.out.println();
+                }
+
+            }
+        } catch (SQLException sqlException) {
+
+            sqlException.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+                statement.close();
+                resultSet.close();
+            } catch (Exception exception) {
+                exception.printStackTrace();
+
+            }
+        }
+
     }
-    
+
 }
