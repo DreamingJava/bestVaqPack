c915530a4ba3df11e82b793502d2c90302982534
chinomatic <chinomatic1970@gmail.com>
2015-11-04 22:00:48 -0600

    Version with wizard example

--- src/vaqpack/VaqPack.java
+++ src/vaqpack/VaqPack.java
@@ -5,83 +5,289 @@
  */
 package vaqpack;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.sql.DriverManager;
-import java.sql.ResultSetMetaData;
-import java.sql.SQLException;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.beans.property.*;
+import javafx.beans.value.*;
+import javafx.collections.*;
+import javafx.event.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
-/**
- *
- * @author OWNER
- */
+import java.util.Stack;
+import static javafx.application.Application.launch;
+
+/** This class displays a survey using a wizard */
 public class VaqPack extends Application {
+  public static void main(String[] args) throws Exception { launch(args); }
+  @Override public void start(Stage stage) throws Exception {
+    // configure and display the scene and stage.
+    stage.setScene(new Scene(new SurveyWizard(stage), 400, 250));
+    stage.show();
+  }
+}
 
-    @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction((ActionEvent event) -> {
-            System.out.println("Hello World!");
-        });
+/** basic wizard infrastructure class */
+class Wizard extends StackPane {
+  private static final int UNDEFINED = -1;
+  private ObservableList<WizardPage> pages = FXCollections.observableArrayList();
+  private Stack<Integer> history = new Stack<>();
+  private int  curPageIdx = UNDEFINED;
 
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+  Wizard(WizardPage... nodes) {
+    pages.addAll(nodes);
+    navTo(0);
+    setStyle("-fx-padding: 10; -fx-background-color: cornsilk;");
+  }
 
-        Scene scene = new Scene(root, 300, 250);
+  void nextPage() {
+    if (hasNextPage()) {
+      navTo(curPageIdx + 1);
+    }
+  }
 
-        primaryStage.setTitle("Hello Dreaming Java");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+  void priorPage() {
+    if (hasPriorPage()) {
+      navTo(history.pop(), false);
+    }
+  }
+
+  boolean hasNextPage() {
+    return (curPageIdx < pages.size() - 1);
+  }
+
+  boolean hasPriorPage() {
+    return !history.isEmpty();
+  }
+
+  void navTo(int nextPageIdx, boolean pushHistory) {
+    if (nextPageIdx < 0 || nextPageIdx >= pages.size()) return;
+    if (curPageIdx != UNDEFINED) {
+      if (pushHistory) {
+        history.push(curPageIdx);
+      }
     }
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        Connection connection = null;
-        Statement statement = null;
-        ResultSet resultSet = null;
-        try {
-            connection = DriverManager.getConnection("jdbc:mysql://localhost/mytest?" + "user=root&password=xeno");
-            statement = connection.createStatement();
-            resultSet = statement.executeQuery("SELECT * FROM untitled");
-            ResultSetMetaData metadata = resultSet.getMetaData();
-            int colCount = metadata.getColumnCount();
-            System.out.println("This is a connection test\n");
-            for (int i = 1; i <= colCount; i++) {
-                System.out.printf("%-8s\t", metadata.getColumnName(i));
-                
-            }
-            while (resultSet.next()) {
-                for (int i = 1; i <= colCount; i++) {
-                    System.out.printf("%-8s\t", resultSet.getObject(i));
-                    System.out.println();
-                }
+    WizardPage nextPage = pages.get(nextPageIdx);
+    curPageIdx = nextPageIdx;
+    getChildren().clear();
+    getChildren().add(nextPage);
+    nextPage.manageButtons();
+  }
 
-            }
-        } catch (SQLException sqlException) {
+  void navTo(int nextPageIdx) {
+    navTo(nextPageIdx, true);
+  }
 
-            sqlException.printStackTrace();
-        } finally {
-            try {
-                connection.close();
-                statement.close();
-                resultSet.close();
-            } catch (Exception exception) {
-                exception.printStackTrace();
+  void navTo(String id) {
+    Node page = lookup("#" + id);
+    if (page != null) {
+      int nextPageIdx = pages.indexOf(page);
+      if (nextPageIdx != UNDEFINED) {
+        navTo(nextPageIdx);
+      }
+    }
+  }
 
-            }
-        }
+  public void finish() {}
+  public void cancel() {}
+}
 
+/** basic wizard page class */
+abstract class WizardPage extends VBox {
+  Button priorButton   = new Button("_Previous");
+  Button nextButton    = new Button("N_ext");
+  Button cancelButton  = new Button("Cancel");
+  Button finishButton  = new Button("_Finish");
+
+  WizardPage(String title) {
+    getChildren().add(LabelBuilder.create().text(title).style("-fx-font-weight: bold; -fx-padding: 0 0 5 0;").build());
+    setId(title);
+    setSpacing(5);
+    setStyle("-fx-padding:10; -fx-background-color: honeydew; -fx-border-color: derive(honeydew, -30%); -fx-border-width: 3;");
+
+    Region spring = new Region();
+    VBox.setVgrow(spring, Priority.ALWAYS);
+    getChildren().addAll(getContent(), spring, getButtons());
+
+    priorButton.setOnAction(new EventHandler<ActionEvent>() {
+      @Override
+      public void handle(ActionEvent actionEvent) {
+        priorPage();
+      }
+    });
+    nextButton.setOnAction(new EventHandler<ActionEvent>() {
+      @Override
+      public void handle(ActionEvent actionEvent) {
+        nextPage();
+      }
+    });
+    cancelButton.setOnAction(new EventHandler<ActionEvent>() {
+      @Override
+      public void handle(ActionEvent actionEvent) {
+        getWizard().cancel();
+      }
+    });
+    finishButton.setOnAction(new EventHandler<ActionEvent>() {
+      @Override
+      public void handle(ActionEvent actionEvent) {
+        getWizard().finish();
+      }
+    });
+  }
+
+  HBox getButtons() {
+    Region spring = new Region();
+    HBox.setHgrow(spring, Priority.ALWAYS);
+    HBox buttonBar = new HBox(5);
+    cancelButton.setCancelButton(true);
+    finishButton.setDefaultButton(true);
+    buttonBar.getChildren().addAll(spring, priorButton, nextButton, cancelButton, finishButton);
+    return buttonBar;
+  }
+
+  abstract Parent getContent();
+
+  boolean hasNextPage() {
+    return getWizard().hasNextPage();
+  }
+
+  boolean hasPriorPage() {
+    return getWizard().hasPriorPage();
+  }
+
+  void nextPage() {
+    getWizard().nextPage();
+  }
+
+  void priorPage() {
+    getWizard().priorPage();
+  }
+
+  void navTo(String id) {
+    getWizard().navTo(id);
+  }
+
+  Wizard getWizard() {
+    return (Wizard) getParent();
+  }
+
+  public void manageButtons() {
+    if (!hasPriorPage()) {
+      priorButton.setDisable(true);
     }
 
+    if (!hasNextPage()) {
+      nextButton.setDisable(true);
+    }
+  }
+}
+
+/** This class shows a satisfaction survey */
+class SurveyWizard extends Wizard {
+  Stage owner;
+  public SurveyWizard(Stage owner) {
+    super(new ComplaintsPage(), new MoreInformationPage(), new ThanksPage());
+    this.owner = owner;
+  }
+  public void finish() {
+    System.out.println("Had complaint? " + SurveyData.instance.hasComplaints.get());
+    if (SurveyData.instance.hasComplaints.get())  {
+      System.out.println("Complaints: " + (SurveyData.instance.complaints.get().isEmpty() ? "No Details" : "\n" + SurveyData.instance.complaints.get()));
+    }
+    owner.close();
+  }
+  public void cancel() {
+    System.out.println("Cancelled");
+    owner.close();
+  }
+}
+
+/** Simple placeholder class for the customer entered survey response. */
+class SurveyData {
+  BooleanProperty hasComplaints = new SimpleBooleanProperty();
+  StringProperty complaints     = new SimpleStringProperty();
+  static SurveyData instance = new SurveyData();
+}
+
+/**
+ * This class determines if the user has complaints.
+ * If not, it jumps to the last page of the wizard.
+ */
+class ComplaintsPage extends WizardPage {
+  private RadioButton yes;
+  private RadioButton no;
+  private ToggleGroup options = new ToggleGroup();
+
+  public ComplaintsPage() {
+    super("Complaints");
+
+    nextButton.setDisable(true);
+    finishButton.setDisable(true);
+    yes.setToggleGroup(options);
+    no.setToggleGroup(options);
+    options.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+      @Override public void changed(ObservableValue<? extends Toggle> observableValue, Toggle oldToggle, Toggle newToggle) {
+        nextButton.setDisable(false);
+        finishButton.setDisable(false);
+      }
+    });
+  }
+
+  Parent getContent() {
+    yes = new RadioButton("Yes");
+    no  = new RadioButton("No");
+    SurveyData.instance.hasComplaints.bind(yes.selectedProperty());
+    return VBoxBuilder.create().spacing(5).children(
+      new Label("Do you have complaints?"), yes, no
+    ).build();
+  }
+
+  void nextPage() {
+    // If they have complaints, go to the normal next page
+    if (options.getSelectedToggle().equals(yes)) {
+      super.nextPage();
+    } else {
+      // No complaints? Short-circuit the rest of the pages
+      navTo("Thanks");
+    }
+  }
+}
+
+/** This page gathers more information about the complaint */
+class MoreInformationPage extends WizardPage {
+  public MoreInformationPage() {
+    super("More Info");
+  }
+
+  Parent getContent() {
+    TextArea textArea = TextAreaBuilder.create().wrapText(true).promptText("Tell me what's wrong Dave...").build();
+    nextButton.setDisable(true);
+    textArea.textProperty().addListener(new ChangeListener<String>() {
+      @Override public void changed(ObservableValue<? extends String> observableValue, String oldValue, String newValue) {
+        nextButton.setDisable(newValue.isEmpty());
+      }
+    });
+    SurveyData.instance.complaints.bind(textArea.textProperty());
+    return VBoxBuilder.create().spacing(5).children(
+      new Label("Please enter your complaints."),
+      textArea
+    ).build();
+  }
+}
+
+/** This page thanks the user for taking the survey */
+class ThanksPage extends WizardPage {
+  public ThanksPage() {
+    super("Thanks");
+  }
+
+  Parent getContent() {
+    StackPane stack = StackPaneBuilder.create().children(
+        new Label("Thanks!")
+    ).build();
+    VBox.setVgrow(stack, Priority.ALWAYS);
+    return stack;
+  }
 }
